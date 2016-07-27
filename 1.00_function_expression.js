d55306277b2f87591e043ee88858c170bef2305b
D. Davis <D. Davis>
2016-07-27 16:12:07 -0400

    inital lessons

---
+++ 1.00_function_expression.js
@@ -0,0 +1,7 @@
+var goingHome = function(){;
+//goingHome and is the varible that is set equal 
+//to a function. The functioin is anonymous
+	console.log("yeah"); 
+};
+
+goingHome();
---
+++ 1.10_modules_and_require.js
@@ -0,0 +1,10 @@
+var students = function(arr){
+	return "There are " + arr.length +  " elements in this array";
+};
+
+console.log(students(["Danyel", "Tara", "Angie", "James"]));
+
+//This returns a string of There are 4 elements in this array
+
+
+
