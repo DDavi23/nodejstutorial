db5d12a6fecd234ee31daacc88fbd7bd4ca4233c
D. Davis <D. Davis>
2016-07-28 09:56:39 -0400

    changes to modules and require

--- 1.10_modules_and_require.js
+++ 1.10_modules_and_require.js
@@ -1,8 +1,8 @@
-var students = function(arr){
+var counter = function(arr){
 	return "There are " + arr.length +  " elements in this array";
 };
 
-console.log(students(["Danyel", "Tara", "Angie", "James"]));
+console.log(counter(["Danyel", "Tara", "Angie", "James"]));
 
 //This returns a string of There are 4 elements in this array
 
