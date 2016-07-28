6f708ccfa51c1e54a31eabae1890443f12abf589
D. Davis <D. Davis>
2016-07-28 10:38:21 -0400

    made changes to app.js

--- app.js
+++ app.js
@@ -5,3 +5,4 @@ console.log(counter(["Danyel", "Tara", "Angie", "James"])); //this is a function
 
 //This module is requiring the info from count.js 
 //and is returning it
+//returns the string There are 4 elements in this array
